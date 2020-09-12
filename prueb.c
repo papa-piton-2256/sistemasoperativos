eb4f874288e1d44abef71d78369fb5000fa47421
compaq <Disraeljm>
2020-09-11 21:11:47 -0500

    version2.0

--- prueb.c
+++ prueb.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <curses.h>
+#include <unistd.h>
 
 /*Definicion de variables globales*/
 int EAX,EBX,ECX,EDX;
@@ -24,13 +25,15 @@ int kbhit(void){
     return 0;
 }
 
-int main(int argc, char const *argv[])
-{
-	char comando[10];
-	char linea[20];
+int main(int argc, char const *argv[]){
+	
+    char comando[10];
+    char linea[20];
     char nombreArchivo[15]="";
 	int cont=1;
-    FILE *fich;
+	char respuesta[2];
+
+    FILE *archivo;
 
     initscr();
 
@@ -44,21 +47,43 @@ int main(int argc, char const *argv[])
             mvprintw(0,0,"(ejecutar/salir)");  
             move(0,20);  
             refresh();
-            
         } 
 
-    if(strcmp(comando,"ejecutar") == 0){
-    fich = fopen("file", "r");
-        while(fgets(linea, 20, (FILE*) fich)) {
-        printf("%d: %s\n", cont, linea);
-        cont++;
-        }  
-    fclose(fich);
-    }
-    if(strcmp(comando,"salir") == 0){
+    	if(strcmp(comando,"ejecutar") == 0){
+    		archivo = fopen(nombreArchivo, "r");
+        	if(archivo == NULL){
+            clear();
+            mvprintw(0,0,"No se pudo arbir el archivo %s\n",nombreArchivo);
+            mvprintw(1,0,"Quieres abrir otro archivo?(S/N)"); 
+            move(1,33);
+            refresh();  
+            mvscanw(1,33,"%s",respuesta);
+          
+            	if(strcmp(respuesta,"N")==0){
+                	endwin();
+                	exit(0);
+            	}else{
+            	clear();
+            	refresh(); 
+            	strcpy(comando," ");
+            	strcpy(nombreArchivo," ");
+            	continue;
+            	}
+            } 
+
+            while(feof(archivo)==0){
+          	fgets(linea,50,archivo); 
+          	printf("%s\n", linea);
+          	sleep(1);
+          	}
+        strcpy(comando," ");
+       	strcpy(nombreArchivo," ");
+    	}
+
+    	if(strcmp(comando,"salir") == 0){
         endwin();
         exit(0);
-    }}
-
+    	}
+	}
 	return 0;
 }
